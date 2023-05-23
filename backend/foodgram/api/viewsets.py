e192c7a51f2b0db1770f1ea98befe51c9727cbc4
a-menshikov <a.menshikov1989@gmail.com>
2023-05-23 18:25:50 +0300

    Добавлены свои вьюсеты

---
+++ backend/foodgram/api/viewsets.py
@@ -0,0 +1,17 @@
+from rest_framework import mixins, viewsets
+# написать докстринги
+
+
+class ListRetriveViewSet(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    viewsets.GenericViewSet
+):
+    pass
+
+
+class ListViewSet(
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet
+):
+    pass

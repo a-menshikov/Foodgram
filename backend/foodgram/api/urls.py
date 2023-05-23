3d6e5ef037d0337e04673988f3c82981fb95f5c3
a-menshikov <a.menshikov1989@gmail.com>
2023-05-23 18:26:00 +0300

    прописаны адреса

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -1,3 +1,11 @@
-# from rest_framework import routers
+from rest_framework import routers
+from django.urls import include, path
+from api.views import IngredientViewSet
 
-# router_v1 = routers.DefaultRouter()
+router_v1 = routers.DefaultRouter()
+
+router_v1.register(r'ingredients', IngredientViewSet, basename='ingredients')
+
+urlpatterns = [
+    path('', include(router_v1.urls)),
+]

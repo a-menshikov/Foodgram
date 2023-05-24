55802694df6797b55953a32a77a28181538ff753
a-menshikov <a.menshikov1989@gmail.com>
2023-05-24 15:12:44 +0300

    концы на юзера и авторизацию

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -1,12 +1,15 @@
 from rest_framework import routers
 from django.urls import include, path
-from api.views import IngredientViewSet, TagViewSet
+from api.views import IngredientViewSet, TagViewSet, CustomUserViewSet
 
 router_v1 = routers.DefaultRouter()
 
 router_v1.register(r'ingredients', IngredientViewSet, basename='ingredients')
 router_v1.register(r'tags', TagViewSet, basename='tags')
+router_v1.register(r'users', CustomUserViewSet)
 
 urlpatterns = [
     path('', include(router_v1.urls)),
+    path('', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
 ]

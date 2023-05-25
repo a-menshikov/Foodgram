269992fa6ed89414b9cbffa2e90359d1e74a121e
a-menshikov <a.menshikov1989@gmail.com>
2023-05-25 19:15:22 +0300

    пермишн автора

---
+++ backend/foodgram/api/permissions.py
@@ -0,0 +1,14 @@
+from rest_framework import permissions
+
+
+class IsAuthor(permissions.BasePermission):
+    """Проверяет авторство пользователя."""
+
+    message = 'У вас недостаточно прав для редактирования!'
+
+    def has_object_permission(self, request, view, obj):
+        return (
+            request.method in permissions.SAFE_METHODS
+            or (request.user.is_authenticated
+                and request.user == obj.author)
+            )

5bfc9cfe030cedd030643ea1133e54e474658260
a-menshikov <a.menshikov1989@gmail.com>
2023-05-25 19:09:46 +0300

    Update urls.py

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -1,12 +1,14 @@
-from rest_framework import routers
+from api.views import (CustomUserViewSet, IngredientViewSet, RecipeViewSet,
+                       TagViewSet)
 from django.urls import include, path
-from api.views import IngredientViewSet, TagViewSet, CustomUserViewSet
+from rest_framework import routers
 
 router_v1 = routers.DefaultRouter()
 
 router_v1.register(r'ingredients', IngredientViewSet, basename='ingredients')
 router_v1.register(r'tags', TagViewSet, basename='tags')
 router_v1.register(r'users', CustomUserViewSet)
+router_v1.register(r'recipes', RecipeViewSet, basename='recipes')
 
 urlpatterns = [
     path('', include(router_v1.urls)),
